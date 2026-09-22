c11cb21471f1bfe1615a7da5c2efedeb72bb6838
TobeChukwu278 <astrolix278@gmail.com>
2026-09-22 15:07:27 +0100

    chore: scaffold project structure with Go server

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Git Sandbox is alive 🚀")
+	})
+
+	log.Println("Server running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
